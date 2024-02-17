9629f6bcda93fa1ecaf6af960ae1ac9524c1d50f
BHushanKothawade2004 <bhushankothawade2004@gmail.com>
2024-02-17 22:08:28 +0530

    Tweet Your Opinion intial

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express(); 
+const port = 3000;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+let posts = [
+    {
+        id: uuidv4(),
+        username: "Franklin",
+        content: "The only thing we have to fear is fear itself."
+    },
+    {
+        id: uuidv4(),
+        username: "Batman",
+        content: "Nights are darkest before dawn",  
+    },
+    {
+        id: uuidv4(),
+        username: "HilaryClinton",
+        content: "Snakes in your backyard won't bite only neighbours",
+    },
+
+]
+
+app.get("/", (req, res) => {
+    res.send("This is our Home Page");
+});
+
+app.get("/tweets", (req, res) => {
+    res.render("tweets.ejs", { posts });
+});
+
+app.get("/tweets/new", (req, res) => {
+    res.render("new.ejs")
+});
+
+app.post("/tweets", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({username, content, id});
+    res.redirect("/tweets"); 
+});
+
+app.get("/tweets/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", { post });
+})
+
+app.patch()
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+}) ; 
