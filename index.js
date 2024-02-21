ae5aa58d792919d58083a1302bc477014a918e50
BHushanKothawade2004 <bhushankothawade2004@gmail.com>
2024-02-21 17:19:39 +0530

    Added a new edit feature for editing tweets

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -57,19 +59,18 @@ app.get("/tweets/:id", (req, res) => {
 
 app.patch("/tweets/:id", (req, res) => {
     let { id } = req.params;
-    let newContent = req.body.Content; 
-    let post = posts.find((p) => id === p.id);
+    let newContent = req.body.content; 
+    let post = posts.find((p) => id === p.id); 
     post.content = newContent;
     console.log(post);
-    res.send("Patch request working"); 
-}) ;
+    res.redirect("/tweets"); 
+});
 
 app.get("/tweets/:id/edit", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", { post });
 });
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
